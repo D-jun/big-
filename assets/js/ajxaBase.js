db7e5b5dc8a56ac9b5a34148fab9e8adbef352b5
D-jun <958419528@qq.com>
2020-11-25 00:31:20 +0800

    完成了后台主页功能

--- assets/js/ajxaBase.js
+++ assets/js/ajxaBase.js
@@ -1,5 +1,22 @@
 $.ajaxPrefilter(function (options) {
   //   console.log(options);
   options.url = "http://ajax.frontend.itheima.net" + options.url;
-  console.log(options.url);
+  // console.log(options.url);
+
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+
+  options.complete = function (xhr) {
+    // console.log(xhr);
+    if (
+      xhr.responseJSON.status === 1 &&
+      xhr.responseJSON.message === "身份认证失败！"
+    ) {
+      localStorage.removeItem("token");
+      location.href = "login.html";
+    }
+  };
 });
